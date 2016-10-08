9ed13d76470730f860e4f462e531acd9b2b9da4e
Aravindyuvi <aravindhanv007@gmail.com>
2016-10-08 23:51:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,11 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
- 
- element.innerHTML = 'newvalue';
- 
-var img = document.getElementById('madi');
-img.onclick() = function() {
-    img.style.marginLeft ='100px';
+
+var button = document.getElementById('main-text');
+var counter = 0;
+button.onclick = function() {
+    
+
+counter = counter + 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+
 };
