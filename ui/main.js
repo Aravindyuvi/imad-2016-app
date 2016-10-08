231f1d53d62a52514ee92fab3053d3c760dac848
Aravindyuvi <aravindhanv007@gmail.com>
2016-10-09 01:04:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,10 @@
 
 var button = document.getElementById('counter');
 
-button.onclick = function() {
+button.onclick = function () {
     
 var requst = new XMLHttpRequest();
+
 request.onreadystatechange = function () {
 if (request.readystate === XMLHttpRequest.DONE) {
 if(request.status === 200) {
@@ -11,7 +12,10 @@ var counter = request.responseText;
 var span = document.getElementById('count');
 span.innerHTML = counter.toStrimg();
 }
+
+    
 }
+
 
 
 };
