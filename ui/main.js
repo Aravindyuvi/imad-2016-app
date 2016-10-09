9bbeca2d4e85b8d95041d2de9ed29be83789af8f
Aravindyuvi <aravindhanv007@gmail.com>
2016-10-09 11:27:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@
 
  request.onreadystatechange = function () {
  if (request.readystate === XMLHttpRequest.DONE) {
- if(request.status === 200) {
+ if (request.status === 200) {
  var counter = request.responseText;
  var span = document.getElementById('count');
  span.innerHTML = counter.toStrimg();
