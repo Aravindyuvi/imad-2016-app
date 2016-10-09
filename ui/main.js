3784b41029ef8b3ce34ca0c405f2b50b11432ffd
Aravindyuvi <aravindhanv007@gmail.com>
2016-10-09 12:52:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,12 @@ request.open('GET', 'http://cloud.imad.hasura.io/counter',true);
 request.send(null);
 
 };
+
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    
+    
+};
